0bafd76e7503b64d4bf590e9bbc09a048ff9197d
泠辰Alena <3024705530@qq.com>
2025-04-21 13:19:58 +0800

    feat: 第六课猜数字游戏作业讲解

--- Lesson6/Homework6.cs
+++ Lesson6/Homework6.cs
@@ -15,71 +15,28 @@ namespace StudyCSharp
             Console.WriteLine("请输入你猜想的数字：");
             string guessNumber = Console.ReadLine();
 
+            // 转换数
             if (float.TryParse(guessNumber, out float toGuessNumber))
             {
+                // 控制循环次数
                 for (int frequency = 1; frequency <= 10; frequency++)
                 {
+                    // 判断猜测的数值是否比设定的小
                     if (number > toGuessNumber)
                     {
-                        Console.WriteLine("太小了");
-                        Console.WriteLine("请继续输入你猜想的数字：");
-                        string guessNumberTwo = Console.ReadLine();
 
-                        if (float.TryParse(guessNumberTwo, out float toguessNumberTwo))
-                        {
-                            if (number < toguessNumberTwo)
-                            {
-                                Console.WriteLine("太大了");
-                            }
-                            else if (number < toguessNumberTwo)
-                            {
-                                Console.WriteLine("太小了");
-                            }
-                            else if (number == toguessNumberTwo)
-                            {
-                                Console.WriteLine("猜对了！");
-                                break;
-                            }
+                    }
 
-                        }
+                    // 判断猜测的数值是否比设定的大
+                    if (number < toGuessNumber)
+                    {
 
-                        else if (number < toGuessNumber)
-                        {
-                            Console.WriteLine("太大了");
-                            Console.WriteLine("请继续输入你猜想的数字：");
+                    }
 
-                            string guessNumberOne = Console.ReadLine();
-                            if (float.TryParse(guessNumberOne, out float toguessNumberOne))
-                            {
-                                if (number < toguessNumberOne)
-                                {
-                                    Console.WriteLine("太大了");
-                                }
-                                else if (number < toguessNumberOne)
-                                {
-                                    Console.WriteLine("太小了");
-                                }
-                                else if (number == toguessNumberOne)
-                                {
-                                    Console.WriteLine("猜对了！");
-                                    break;
-                                }
-                            }
-
-                        }
-
-                        else if (number == toGuessNumber)
-                        {
-                            Console.WriteLine("猜对了！");
-                            break;
-                        }
-
-
-                        else
-                        {
-                            Console.WriteLine("输入错误！");
-                        }
-                        Console.WriteLine($"你还剩：{10 - frequency}次机会");
+                    // 判断猜测的数值是否与设定相等
+                    if (number == toGuessNumber)
+                    {
+                        
                     }
 
                     Console.ReadKey();
