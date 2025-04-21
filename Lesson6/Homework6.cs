840aba1538467973013f68965f3b9728f9762277
yangqiong520 <3441723843@qq.com>
2025-04-21 13:19:28 +0800

    99乘法表

--- Lesson6/Homework6.cs
+++ Lesson6/Homework6.cs
@@ -10,79 +10,13 @@ namespace StudyCSharp
     {
         static public void Homework()
         {
-            Console.WriteLine("欢迎来到猜数字游戏，你一共有十次机会！");
-            float number = 55;
-            Console.WriteLine("请输入你猜想的数字：");
-            string guessNumber = Console.ReadLine();
-
-            if (float.TryParse(guessNumber, out float toGuessNumber))
+            int[] number = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            foreach (int numberOne in number)
             {
-                for (int frequency = 1; frequency <= 10; frequency++)
+                foreach (int numberTwo in number)
                 {
-                    if (number > toGuessNumber)
-                    {
-                        Console.WriteLine("太小了");
-                        Console.WriteLine("请继续输入你猜想的数字：");
-                        string guessNumberTwo = Console.ReadLine();
 
-                        if (float.TryParse(guessNumberTwo, out float toguessNumberTwo))
-                        {
-                            if (number < toguessNumberTwo)
-                            {
-                                Console.WriteLine("太大了");
-                            }
-                            else if (number < toguessNumberTwo)
-                            {
-                                Console.WriteLine("太小了");
-                            }
-                            else if (number == toguessNumberTwo)
-                            {
-                                Console.WriteLine("猜对了！");
-                                break;
-                            }
-
-                        }
-
-                        else if (number < toGuessNumber)
-                        {
-                            Console.WriteLine("太大了");
-                            Console.WriteLine("请继续输入你猜想的数字：");
-
-                            string guessNumberOne = Console.ReadLine();
-                            if (float.TryParse(guessNumberOne, out float toguessNumberOne))
-                            {
-                                if (number < toguessNumberOne)
-                                {
-                                    Console.WriteLine("太大了");
-                                }
-                                else if (number < toguessNumberOne)
-                                {
-                                    Console.WriteLine("太小了");
-                                }
-                                else if (number == toguessNumberOne)
-                                {
-                                    Console.WriteLine("猜对了！");
-                                    break;
-                                }
-                            }
-
-                        }
-
-                        else if (number == toGuessNumber)
-                        {
-                            Console.WriteLine("猜对了！");
-                            break;
-                        }
-
-
-                        else
-                        {
-                            Console.WriteLine("输入错误！");
-                        }
-                        Console.WriteLine($"你还剩：{10 - frequency}次机会");
-                    }
-
-                    Console.ReadKey();
+                    Console.Write($"{numberOne}*{numberTwo}={numberTwo * numberOne} ");
 
                 }
             }
